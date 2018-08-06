7dc8ab4a2128566b3c4610dd334452fd35c348a2
Rebecca Horwitz <rebeccahorwitz@Rebeccas-MacBook-Pro.local>
2018-08-06 11:48:21 -0700

    set up app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,12 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
 
-module.exports = {mongoose};
+// let db = {
+//     localhost: 'mongodb://localhost:27017/TodoApp',
+//     mlab: 'mongodb://test:testing123@ds018568.mlab.com:18568/todo-app-api'
+// };
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+// mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+module.exports = { mongoose };
